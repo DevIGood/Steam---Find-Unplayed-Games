c1b04dd0efca3d045d574e84522f5dd78c152c98
DevIGood <markiz948@gmail.com>
2022-11-05 02:27:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## About
+
+Most of us have a vast Steam library with games that will never be played.
+This is just a thing that i created to force myself to play games in my Steam library.
+
+## Running the program
+
+You will need to get your own Steam API and Steam ID.
+
+Steam API can be aquired [here](https://steamcommunity.com/dev) and Steam ID can be found on your profile page in the URL.
+
+Remove the 1st line and add **key** and **SteamID** strings.
+
+## Note
+
+Run the update list function atleast ones for other functions to work.
---
+++ main.py
@@ -0,0 +1,58 @@
+from exclude import privateInfo
+import requests
+import random
+import json
+import os
+
+def readJson():
+    with open("unplayed_games.json", "r") as file:
+        jsonText = json.load(file)
+    file.close()
+    return jsonText
+
+def writeJson(jsonText):
+    with open("unplayed_games.json", "w") as file:
+        file.write(jsonText)
+    file.close()
+
+def printGameList():
+    jsonText = readJson()
+    for x in range(len(jsonText['response']['games'])):
+        print(jsonText['response']['games'][x]['name'],)
+
+def pickRandomGame():
+    randGame = []
+    jsonText = readJson()
+    for x in range(len(jsonText['response']['games'])):
+        randGame.append(jsonText['response']['games'][x]['name'],)
+    print(random.choice(randGame))
+
+def updateList():
+    key = privateInfo.get_Key()
+    SteamID = privateInfo.get_SteamID()
+    URL = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={SteamID}&include_appinfo=1&format=json"
+    req = requests.get(url = URL)
+    jsonText = req.json()
+    for x in reversed(jsonText['response']['games']):
+        if (x['playtime_forever'] != 0):
+            jsonText['response']['games'].remove(x)
+    jsonText = json.dumps(jsonText, indent=4)
+    writeJson(jsonText)
+
+while True:
+    choice = int(input("\n1)Print games you haven't played\n2)Pick random game to play\n3)Update list\n4)Exit\n\nYour choice = "))
+    if choice == 1:
+        os.system('cls')
+        printGameList()
+    elif choice == 2:
+        os.system('cls')
+        pickRandomGame()
+    elif choice == 3:
+        os.system('cls')
+        updateList()
+    elif choice == 4:
+        os.system('cls')
+        break
+    else:
+        os.system('cls')
+        print("Wrong input")
